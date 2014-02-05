752aaf95f4ad336e3829887dfbead0d6295dd186
Robbie Hanson <robbiehanson@deusty.com>
2014-02-05 14:56:47 -0800

    Minor (inconsequential) changes.

--- TestXcodeColors/main.m
+++ TestXcodeColors/main.m
@@ -1,12 +1,5 @@
-//
-//  main.m
-//  TestXcodeColors
-//
-//  Created by Robbie Hanson on 5/22/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
+
 
 int main(int argc, char *argv[])
 {
